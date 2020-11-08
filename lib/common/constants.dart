e2c0c5eb712e3d63e1d740036225eadc25d6ebb5
Jason Poindexter <poindexter.json@gmail.com>
2020-11-08 11:48:53 -0800

    Add OpenWeather req for zip based weather (F)

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,3 +1,5 @@
 class Constants {
   static const String appName = 'Weather';
+  static const String openWeatherUri = 'api.openweathermap.org';
+  static const String openWeatherApiId = 'OPENWEATHER_API_ID';
 }
